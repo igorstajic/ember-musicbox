a53b6754746eaa39dcc5b54900f7eae47250a365
Igor Stajic <istajic@aortasoftware.com>
2016-06-30 11:31:37 +0200

    Added mirage mocks

---
+++ app/models/song.js
@@ -0,0 +1,12 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  name: attr('string'),
+  genre: attr('string'),
+  duration: attr('number'),
+  hearts: attr('number'),
+  artist: attr(),
+  album: attr()
+});
---
+++ mirage/fixtures/songs.js
@@ -0,0 +1,113 @@
+export default [{
+  "id": 0,
+  "name": "Song 2",
+  "duration": 121000,
+  "genre": "Indie",
+  "hearts": 2,
+  "artist": {
+    "name": "Blur"
+  },
+  "album": {
+    "name": "Blur",
+    "year": 1997,
+    "genre": "Indie"
+  }
+}, {
+  "id": 1,
+  "name": "Lonesome Street",
+  "duration": 262000,
+  "genre": "Alternative/Indie",
+  "hearts": 0,
+  "artist": {
+    "name": "Blur"
+  },
+  "album": {
+    "name": "The Magic Whip",
+    "year": 2015,
+    "genre": "Alternative/Indie"
+  }
+}, {
+  "id": 2,
+  "name": "I Am The Walrus",
+  "duration": 275000,
+  "genre": "Rock",
+  "hearts": 1,
+  "artist": {
+    "name": "The Beatles"
+  },
+  "album": {
+    "name": "Magical Mystery Tour",
+    "year": 1967,
+    "genre": "Rock"
+  }
+}, {
+  "id": 3,
+  "name": "Come Together",
+  "duration": 259000,
+  "genre": "Rock",
+  "hearts": 2,
+  "artist": {
+    "name": "The Beatles"
+  },
+  "album": {
+    "name": "Abbey Road",
+    "year": 1969,
+    "genre": "Rock"
+  }
+}, {
+  "id": 4,
+  "name": "Purple Haze",
+  "duration": 170000,
+  "genre": "Classic Rock",
+  "hearts": 3,
+  "artist": {
+    "name": "The Jimi Hendrix Experience"
+  },
+  "album": {
+    "name": "Are You Experienced",
+    "year": 1967,
+    "genre": "Classic Rock"
+  }
+}, {
+  "id": 5,
+  "name": "Bright Lights",
+  "duration": 324000,
+  "genre": "Blues",
+  "hearts": 3,
+  "artist": {
+    "name": "Gary Clark Jr."
+  },
+  "album": {
+    "name": "Blak And Blu",
+    "year": 2012,
+    "genre": "Blues"
+  }
+}, {
+  "id": 6,
+  "name": "Ain't Messin 'Round",
+  "duration": 249000,
+  "genre": "Blues",
+  "hearts": 2,
+  "artist": {
+    "name": "Gary Clark Jr."
+  },
+  "album": {
+    "name": "Blak And Blu",
+    "year": 2012,
+    "genre": "Blues"
+  }
+}, {
+  "id": 7,
+  "name": "The Healing",
+  "duration": 288000,
+  "genre": "Rock",
+  "hearts": 3,
+  "artist": {
+    "name": "Gary Clark Jr."
+  },
+  "album": {
+    "name": "The Story of Sunny Boy Slim",
+    "year": 2015,
+    "genre": "Rock"
+  }
+}];
