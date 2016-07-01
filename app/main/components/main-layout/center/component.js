9a87fc72cb548fcceee893e82e2693ddf4156139
Igor Stajic <istajic@aortasoftware.com>
2016-07-01 10:46:56 +0200

    More refactoring

---
+++ app/main/components/main-layout/center/component.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames:['col-md-9 col-sm-8 col-xs-8']
+});
---
+++ app/main/components/main-layout/side/component.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['side-container col-md-3 col-md-offset-9 col-sm-4 col-sm-offset-8 col-xs-4 col-xs-offset-8']
+});
