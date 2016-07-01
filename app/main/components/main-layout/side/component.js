a8227198f486af854bb0736b5fd7093585cb47ad
Igor Stajic <istajic@aortasoftware.com>
2016-07-01 10:58:48 +0200

    Final updates.

--- app/main/components/main-layout/side/component.js
+++ app/main/components/main-layout/side/component.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
-
-export default Ember.Component.extend({
+const {
+  Component
+} = Ember;
+export default Component.extend({
   classNames: ['side-container col-md-3 col-md-offset-9 col-sm-4 col-sm-offset-8 col-xs-4 col-xs-offset-8']
 });
